0ec29081d4b8de9dee45c76988d422003775a017
init.defaultBranch <benlimpic@gmail.com>
2022-03-28 10:33:19 -0700

    new branch commit

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ fetch("https://www.superheroapi.com/api.php/1955715441283288/222")
     .then(res => res.json())
     .then(superCharacter => buildChar(superCharacter))
 
-    const buildChar
+const buildChar = (character) => {
+    const characterBox = createElement('div')
+    const characterImage = createElement('img')
+    const characterName = createElement('h2')
+    
+}
 
 
 
