bcc7383d41f785b9b9722779b47013dcfe8f46b1
init.defaultBranch <benlimpic@gmail.com>
2022-03-28 10:23:05 -0700

    connected index.js and successful fetch of API single character

---
+++ index.js
@@ -0,0 +1,9 @@
+
+fetch("https://www.superheroapi.com/api.php/1955715441283288/222")
+    .then(res => res.json())
+    .then(superCharacter => buildChar(superCharacter))
+
+    const buildChar
+
+
+
